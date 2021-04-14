37ea40cdde28722ba1daf5f2907ca664f1590360
ItsCxyy <reto@cxyy.dev>
2021-04-14 21:45:54 +0200

    Add TypeScript support for VueJs

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,10 +6,10 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default Vue.extend({
+export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
