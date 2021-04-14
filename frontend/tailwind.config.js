08a7fd13337d8af7e0218a23fadf13e0cbea4e50
Titus Kirch <requests@tkirch.dev>
2021-04-14 22:21:52 +0200

    #22 add tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './src/**/*.vue',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
