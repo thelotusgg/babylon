7c3d92d9038950814e26c7ac23dbdbd7c046bf8e
Sofaklient <johannes@nowinski.de>
2021-04-15 20:59:02 +0200

    #54 manually fix remaining linting errors

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
+export default class AppService {
+  static getHello(): string {
     return 'Hello World!';
   }
 }
