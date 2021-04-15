020e017474ea219c507f5e5d1d880a954e01087f
Sofaklient <johannes@nowinski.de>
2021-04-15 20:59:02 +0200

    #54 manually fix remaining linting errors

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-typescript/base',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
+    project: './tsconfig.json'
   },
   plugins: [
     '@typescript-eslint',
