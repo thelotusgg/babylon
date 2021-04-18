fc167fab6ac4a17abd09f299483cb5bdbe46e35d
Titus Kirch <requests@tkirch.dev>
2021-04-18 22:14:37 +0200

    #82 rework and cleanup of frontend

---
+++ frontend/src/components/AppNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="bg-gray-900 w-full flex items-center lg:px-4"
+  :class="menuButtonPosition == 'right' ? 'flex-row-reverse' : ''">
+
+    <router-link class="flex-grow lg:flex-grow-0 text-4xl text-gray-50 uppercase
+    p-2 text-center lg:text-left" :to="{name: 'dashboard'}">{{ $t(appName) }}</router-link>
+
+    <div class="p-2 group hover:cursor-pointer absolute lg:invisible">
+      <font-awesome-icon :icon="['fas', 'bars']" class="text-4xl text-gray-200
+      group-hover:text-green-500"/>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Navbar',
+  props: {
+    appName: {
+      type: String,
+      default: 'app.name',
+    },
+    menuButtonPosition: {
+      type: String,
+      validator: (value: 'left'|'right'): boolean => ['left', 'right'].indexOf(value) !== -1,
+      default: 'left',
+    },
+  },
+};
+</script>
