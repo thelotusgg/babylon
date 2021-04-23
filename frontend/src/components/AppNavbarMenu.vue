9c6d2dc8999ac9e9ab81121ed2da876a123e7266
Titus Kirch <requests@tkirch.dev>
2021-04-23 16:50:27 +0200

    #103 make navbar 2/5 wide on small screens

--- frontend/src/components/AppNavbarMenu.vue
+++ frontend/src/components/AppNavbarMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="flex flex-col bg-gray-700 w-full h-screen transform duration-300
+  <ul class="min-w-2/5 w-auto flex flex-col bg-gray-700 h-screen transform duration-300
     absolute lg:bg-transparent lg:static lg:flex-row lg:w-auto lg:h-auto">
     <slot/>
   </ul>
