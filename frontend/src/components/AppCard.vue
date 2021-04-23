f95e83a888b2d3a6e3a527fc19bbda48927126cd
Titus Kirch <requests@tkirch.dev>
2021-04-23 16:59:23 +0200

    #107 add basic dark mode

--- frontend/src/components/AppCard.vue
+++ frontend/src/components/AppCard.vue
@@ -1,18 +1,19 @@
 <template>
   <div
-    class="group flex bg-gray-50 shadow-sm p-4 border-l-4 relative gap-4 transition-shadow
-    duration-500 hover:shadow-md"
+    class="group flex bg-gray-50 dark:bg-gray-800 shadow-sm p-4 border-l-4 relative gap-4
+    transition-shadow duration-500 hover:shadow-md"
     :class="'border-' + color">
     <div class="flex-grow">
-      <h1 v-if="title" class="text-gray-700 text-2xl">{{ title }}</h1>
-      <p v-if="description" class="text-gray-600">
+      <h1 v-if="title" class="text-gray-700 dark:text-gray-200 text-2xl">{{ title }}</h1>
+      <p v-if="description" class="text-gray-600 dark:text-gray-200 ">
         {{ description }}
       </p>
     </div>
     <div class="flex items-center">
-      <font-awesome-icon :icon="['fas', 'chevron-right']" class="text-3xl text-gray-300
-        transition-transform duration-500 transform group-hover:translate-x-2
-        group-hover:text-gray-700"/>
+      <font-awesome-icon
+      :icon="['fas', 'chevron-right']"
+      class="text-3xl text-gray-300 transition-transform duration-500 transform
+      group-hover:translate-x-2 group-hover:text-gray-700 dark:group-hover:text-gray-50"/>
     </div>
     <router-link
       v-if="to"
