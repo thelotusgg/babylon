7a9b4651f92a572cd6ab67c462a26e33561b893e
Titus <tituskirch1999@gmail.com>
2021-05-30 16:25:47 +0200

    add project resolvers

--- backend-express/src/resolvers/project.ts
+++ backend-express/src/resolvers/project.ts
@@ -1,4 +1,12 @@
+/* eslint-disable max-len, no-underscore-dangle */
+import { getMultiligualTextResolver } from '../utils';
+
 export default {
+  Project: {
+    category: (parent: any, __: any, { dataSources: { category } }: any) => category.get(parent.category),
+    description: async (parent: any, { languageCode, fallback }: any, context: any) => getMultiligualTextResolver(parent.descriptions, languageCode, fallback, context),
+    title: async (parent: any, { languageCode, fallback }: any, context: any) => getMultiligualTextResolver(parent.titles, languageCode, fallback, context),
+  },
   Query: {
     projects: async (_: any, __: any, { dataSources: { project } }: any) => project.getAll(),
     project: (_: any, { _id }: any, { dataSources: { project } }: any) => project.get(_id),
