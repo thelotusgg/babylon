5b27c90c905439e562ff83ff9a79317230cc5894
Titus <tituskirch1999@gmail.com>
2021-06-03 13:29:20 +0200

    add missing exec()

--- backend-express/src/dataLoaders/phraseLoader.ts
+++ backend-express/src/dataLoaders/phraseLoader.ts
@@ -2,6 +2,6 @@ import DataLoader from 'dataloader';
 import { Phrase } from '../models';
 
 // eslint-disable-next-line max-len
-const phraseLoader = new DataLoader(async (phraseIds) => Phrase.find({ _id: { $in: phraseIds } }).exec());
+const phraseLoader = new DataLoader(async (phraseIds) => Phrase.find({ _id: { $in: phraseIds } }));
 
 export default phraseLoader;
