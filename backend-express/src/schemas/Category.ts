54e922b93cbcab3ed4012be4b7a22dbaff756ba5
Titus <tituskirch1999@gmail.com>
2021-06-05 17:27:14 +0200

    require titles value

--- backend-express/src/schemas/Category.ts
+++ backend-express/src/schemas/Category.ts
@@ -1,16 +1,20 @@
 import { Schema } from 'mongoose';
 
 export default new Schema({
-  titles: [{
-    languageCode: {
-      type: String,
-      required: true,
-    },
-    value: {
-      type: String,
-      required: true,
-    },
-  }],
+  titles: {
+    type: [{
+      languageCode: {
+        type: String,
+        required: true,
+      },
+      value: {
+        type: String,
+        required: true,
+      },
+    }],
+    required: true,
+    validate: (x: any) => Array.isArray(x) && x.length > 0,
+  },
   descriptions: [{
     languageCode: {
       type: String,
